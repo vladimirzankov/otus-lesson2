f6e5dc19b24b941dded12837eede8fa9ce551d4b
vladimirzankov <vladimirzankov@outlook.com>
2019-06-08 16:21:13 +0300

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Test1 {
+    public static final Logger logger = LogManager.getLogger(Test1.class);
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://otus.ru");
+        logger.info("Open otus.ru");
+    }
+}
